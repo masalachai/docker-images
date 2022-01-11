d2e09019eaf94c69cda6c602b1855bb63707f5b2
Ritesh Chitlangi <ritesh@ayravat.com>
2022-01-11 16:45:43 +0800

    refactor for multiple registries

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for f in $(git diff --name-only --diff-filter=d HEAD^); do
+	NS=$(echo "$f" | awk -F '/' '{print $2}');
+	if [ "$NS" = ".drone.yml" ] || [ "$NS" = "build.sh" ]; then continue; fi;
+	NAME=$(echo "$f" | awk -F '/' '{print $3}');
+	VER=$(echo "$f" | awk -F '/' '{print $4}' | sed -e 's/Dockerfile-//');
+	TAG="$NS/$NAME:$VER";
+	echo "========== Building $TAG ==========";
+	docker build -t $TAG -f "$f" .;
+	docker --config "/root/.docker/$NS" push "$TAG";
+done
+

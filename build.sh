a52f65b5219c0b52ce9bf7dbd9c6bcad4ef2e5cb
Ritesh Chitlangi <ritesh@ayravat.com>
2022-01-11 16:48:39 +0800

    fix build script bug

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 for f in $(git diff --name-only --diff-filter=d HEAD^); do
 	NS=$(echo "$f" | awk -F '/' '{print $2}');
-	if [ "$NS" = ".drone.yml" ] || [ "$NS" = "build.sh" ]; then continue; fi;
+	if [ "$NS" = ".drone.yml" ] || [ "$NS" = "build.sh" ] || [ "$NS" = "" ]; then continue; fi;
 	NAME=$(echo "$f" | awk -F '/' '{print $3}');
 	VER=$(echo "$f" | awk -F '/' '{print $4}' | sed -e 's/Dockerfile-//');
 	TAG="$NS/$NAME:$VER";
